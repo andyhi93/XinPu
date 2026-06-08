240ec04dfb795309ecc7e4a1fe178665ac7050bd
lougan <134981472+andyhi93@users.noreply.github.com>
2026-06-08 10:31:22 +0800

    系統 3：地點系統 (UI判定完成)

---
+++ Assets/Script/LocationHotspot.cs
@@ -0,0 +1,38 @@
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class LocationHotspot : MonoBehaviour,
+    IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [Header("地點設定")]
+    public LocationManager.Location location;
+
+    [Header("UI 引用")]
+    public GameObject border;      // 選中外框
+    public GameObject label;       // 地點名稱
+    public GameObject eventDot;    // 事件提示點 (可選)
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        if (border != null) border.SetActive(true);
+        if (label != null) label.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData e)
+    {
+        if (border != null) border.SetActive(false);
+        if (label != null) label.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        if (LocationManager.Instance != null)
+        {
+            LocationManager.Instance.GoToLocation(location);
+        }
+        else
+        {
+            Debug.LogError("【地點系統】找不到 LocationManager 實例！");
+        }
+    }
+}
